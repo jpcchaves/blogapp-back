a3c120420742c6d536643f4c9354c2ff20396fef
João Paulo <jpcchaves@outlook.com>
2023-03-09 22:16:12 -0300

    feat: add comment dto

---
+++ src/main/java/com/jpcchaves/blogapp/payload/CommentDto.java
@@ -0,0 +1,11 @@
+package com.jpcchaves.blogapp.payload;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
+}
