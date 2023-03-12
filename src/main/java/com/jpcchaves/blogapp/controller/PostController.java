4e696292bb8d6503ec57110e0b2e519de8e4887d
João Paulo <jpcchaves@outlook.com>
2023-03-12 16:31:04 -0300

    fix: resolving bug authorizing roles

--- src/main/java/com/jpcchaves/blogapp/controller/PostController.java
+++ src/main/java/com/jpcchaves/blogapp/controller/PostController.java
@@ -7,11 +7,11 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/posts")
+@RequestMapping()
 public class PostController {
 
     private PostService postService;
@@ -20,31 +20,32 @@ public class PostController {
         this.postService = postService;
     }
 
-    @GetMapping
-    public ResponseEntity<PostResponse> getAll(Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(postService.getAll(pageable));
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<PostDto> getById(@PathVariable(value = "id") Long id) {
-        return new ResponseEntity<>(postService.getById(id), HttpStatus.OK);
-    }
-
-    @Secured("ROLE_ADMIN")
-    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> create(@Valid @RequestBody PostDto request) {
         return new ResponseEntity<>(postService.create(request), HttpStatus.CREATED);
     }
 
-    @Secured("ROLE_ADMIN")
-    @PutMapping(value = "/{id}")
+    @GetMapping("/api/v1/posts")
+    public ResponseEntity<PostResponse> getAll(Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.getAll(pageable));
+    }
+
+    @GetMapping(value = "/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getById(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<>(postService.getById(id), HttpStatus.OK);
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> update(@PathVariable(value = "id") Long id,
                                           @Valid @RequestBody PostDto request) {
         return new ResponseEntity<>(postService.update(id, request), HttpStatus.OK);
     }
 
-    @Secured("ROLE_ADMIN")
-    @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         postService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
