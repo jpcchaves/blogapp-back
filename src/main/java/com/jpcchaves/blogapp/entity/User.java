bd0d287e8de9a8fa05c3bc7f9c5fabbe3ed855cb
João Paulo <jpcchaves@outlook.com>
2023-03-12 13:01:18 -0300

    feat: add many to many relation between user and roles

--- src/main/java/com/jpcchaves/blogapp/entity/User.java
+++ src/main/java/com/jpcchaves/blogapp/entity/User.java
@@ -2,6 +2,8 @@ package com.jpcchaves.blogapp.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -19,6 +21,13 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 
     public User() {
     }
