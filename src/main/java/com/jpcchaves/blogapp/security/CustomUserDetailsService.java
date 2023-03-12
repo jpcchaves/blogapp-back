59132a95c8cee99f949865fde0af8f5ae18b44e6
João Paulo <jpcchaves@outlook.com>
2023-03-12 16:17:13 -0300

    fix: resolving secured endpoint bug @Secured('ROLE_ADMIN')

--- src/main/java/com/jpcchaves/blogapp/security/CustomUserDetailsService.java
+++ src/main/java/com/jpcchaves/blogapp/security/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.jpcchaves.blogapp.Security;
+package com.jpcchaves.blogapp.security;
 
 import com.jpcchaves.blogapp.entity.User;
 import com.jpcchaves.blogapp.repository.UserRepository;
@@ -25,21 +25,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
         User user = repository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException(
-                                "User not found with username or email: " + usernameOrEmail
-                        )
-                );
+                        new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
 
-        // Convert set of Role to a set of SimpleGrantedAuthorities
         Set<GrantedAuthority> authorities = user
                 .getRoles()
                 .stream()
-                .map((role) ->
-                        new SimpleGrantedAuthority(role.getName())
-                )
-                .collect(Collectors.toSet());
+                .map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
 
-
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                authorities);
     }
 }
