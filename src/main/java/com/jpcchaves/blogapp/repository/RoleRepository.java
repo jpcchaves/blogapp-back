12c437f2c49094e931468c8503826d4fc27c38ca
João Paulo <jpcchaves@outlook.com>
2023-03-12 13:08:18 -0300

    feat: add role repository

---
+++ src/main/java/com/jpcchaves/blogapp/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.jpcchaves.blogapp.repository;
+
+import com.jpcchaves.blogapp.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
