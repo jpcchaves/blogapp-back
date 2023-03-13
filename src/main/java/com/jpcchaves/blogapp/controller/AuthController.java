1c8baa1b978e4a60ea16ea33c4c3b3ee7352abdf
João Paulo <jpcchaves@outlook.com>
2023-03-13 13:06:49 -0300

    feat: add auth controller

---
+++ src/main/java/com/jpcchaves/blogapp/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.jpcchaves.blogapp.controller;
+
+import com.jpcchaves.blogapp.payload.LoginDto;
+import com.jpcchaves.blogapp.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.login(loginDto));
+    }
+}
--- src/main/java/com/jpcchaves/blogapp/service/impl/AuthServiceImpl.java
+++ src/main/java/com/jpcchaves/blogapp/service/impl/AuthServiceImpl.java
@@ -1,11 +1,9 @@
 package com.jpcchaves.blogapp.service.impl;
 
 import com.jpcchaves.blogapp.payload.LoginDto;
-import com.jpcchaves.blogapp.repository.UserRepository;
 import com.jpcchaves.blogapp.service.AuthService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private AuthenticationManager authenticationManager;
-
+    
     public AuthServiceImpl() {
     }
 
@@ -23,9 +21,9 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String login(LoginDto loginDto) {
-       var authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+        var authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-       return "User logged in successfully";
+        return "User logged in successfully";
     }
 }
