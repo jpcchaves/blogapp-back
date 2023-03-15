4cfc7e31816d02d616928df9d21e4c84420c7b45
João Paulo <jpcchaves@outlook.com>
2023-03-15 12:45:59 -0300

    feat: add category crate controller

---
+++ src/main/java/com/jpcchaves/blogapp/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.jpcchaves.blogapp.controller;
+
+import com.jpcchaves.blogapp.payload.CategoryDto;
+import com.jpcchaves.blogapp.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto categoryDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(categoryDto));
+    }
+}
