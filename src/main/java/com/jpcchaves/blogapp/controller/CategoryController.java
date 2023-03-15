ecae1ef439214b801d0c2dd51c51f9b7021a6db0
João Paulo <jpcchaves@outlook.com>
2023-03-15 13:38:19 -0300

    feat: add get category by id

--- src/main/java/com/jpcchaves/blogapp/controller/CategoryController.java
+++ src/main/java/com/jpcchaves/blogapp/controller/CategoryController.java
@@ -5,10 +5,7 @@ import com.jpcchaves.blogapp.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -23,5 +20,10 @@ public class CategoryController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> create(@RequestBody CategoryDto categoryDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(categoryDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getById(id));
     }
 }
