f1c9738701c78549145259a6a506b67bb4358481
João Paulo <jpcchaves@outlook.com>
2023-03-17 13:28:40 -0300

    feat: add docs to post controller

--- src/main/java/com/jpcchaves/blogapp/controller/PostController.java
+++ src/main/java/com/jpcchaves/blogapp/controller/PostController.java
@@ -3,7 +3,11 @@ package com.jpcchaves.blogapp.controller;
 import com.jpcchaves.blogapp.payload.PostDto;
 import com.jpcchaves.blogapp.payload.PostResponse;
 import com.jpcchaves.blogapp.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -15,6 +19,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping()
+@Tag(
+        name = "REST Controller for Post Resource"
+)
 public class PostController {
 
     private PostService postService;
@@ -23,6 +30,15 @@ public class PostController {
         this.postService = postService;
     }
 
+    @Operation(
+            summary = "Creates a Post",
+            description = "Creates a Post"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http status 201 CREATED",
+            content = @Content(mediaType = "application/json")
+    )
     @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/api/v1/posts")
@@ -30,17 +46,44 @@ public class PostController {
         return new ResponseEntity<>(postService.create(request), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get a list of Posts",
+            description = "Get a list of all Posts"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 OK",
+            content = @Content(mediaType = "application/json")
+    )
     @GetMapping("/api/v1/posts")
     public ResponseEntity<PostResponse> getAll(Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(postService.getAll(pageable));
     }
 
+    @Operation(
+            summary = "Get a Post",
+            description = "Get a Post by passing his ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 OK",
+            content = @Content(mediaType = "application/json")
+    )
     @GetMapping(value = "/api/v1/posts/{id}")
     public ResponseEntity<PostDto> getById(@PathVariable(value = "id") Long id) {
         return new ResponseEntity<>(postService.getById(id), HttpStatus.OK);
     }
 
 
+    @Operation(
+            summary = "Updates a Post",
+            description = "Updates a Post"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 OK",
+            content = @Content(mediaType = "application/json")
+    )
     @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/api/v1/posts/{id}")
@@ -49,6 +92,14 @@ public class PostController {
         return new ResponseEntity<>(postService.update(id, request), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Deletes a Post",
+            description = "Deletes a Post"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "Http status 204 NO CONTENT"
+    )
     @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/api/v1/posts/{id}")
@@ -57,6 +108,16 @@ public class PostController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(
+            summary = "Gets a list of posts by passing a category ID",
+            description = "Gets a list of posts by passing a category ID"
+
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 OK",
+            content = @Content(mediaType = "application/json")
+    )
     @GetMapping("/api/v1/posts/category/{categoryId}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("categoryId") Long categoryId) {
         var posts = postService.getPostsByCategory(categoryId);
